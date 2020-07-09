fbda90474fbde84b526e1bcbd376c70aeae5b927
Андрей <perehodkinandriy@gmail.com>
2020-07-09 23:15:35 +0300

    Its everything

--- src/ExerciseTwo.java
+++ src/ExerciseTwo.java
@@ -7,49 +7,50 @@ import java.util.List;
 
 public class ExerciseTwo {
     List<String> lines;
-    List<Transaction> transactions= new ArrayList<>();
+    List<Transaction> transactions = new ArrayList<>();
 
     ExerciseTwo(List<String> lines) {
         this.lines = lines;
     }
 
-    void addInformationOfTransaction(){
-        int currentTransaction=1;
-        int numberOfTransaction=0;
-        for (String line: lines) {
-            if(line.contains("Transaction "+(1+numberOfTransaction)+" begin:")) {
-                String tempDate = line.substring(line.indexOf("[")+1,line.indexOf("]"));
+    void addInformationOfTransaction() {
+        int currentTransaction = 1;
+        int numberOfTransaction = 0;
+        for (String line : lines) {
+            if (line.contains("Transaction " + (1 + numberOfTransaction) + " begin:")) {
+                String tempDate = line.substring(line.indexOf("[") + 1, line.indexOf("]"));
                 transactions.add(new Transaction());
                 transactions.get(numberOfTransaction).setDate(tempDate);
                 numberOfTransaction++;
             }
-            if(line.contains("Transaction "+currentTransaction)&&line.contains("end")){
+            if (line.contains("Transaction " + currentTransaction) && line.contains("end")) {
                 currentTransaction++;
             }
-            if(line.contains("EMV Scheme Matched:")&& numberOfTransaction>=currentTransaction){
-                if(transactions.get(currentTransaction-1).getEMV_Scheme().equals("")) {
-                    String tempEMV_Scheme = line.substring(line.indexOf("[")+1, line.indexOf("]"));
+            if (line.contains("EMV Scheme Matched:") && numberOfTransaction >= currentTransaction) {
+                if (transactions.get(currentTransaction - 1).getEMV_Scheme().equals("")) {
+                    String tempEMV_Scheme = line.substring(line.indexOf("[") + 1, line.indexOf("]"));
                     transactions.get(currentTransaction - 1).setEMV_Scheme(tempEMV_Scheme);
                 }
             }
         }
     }
 
-    void checkTypeOfApp(String date,String EMV_Scheme){
+    String checkTypeOfApp(String date, String EMV_Scheme) throws IllegalArgumentException {
         addInformationOfTransaction();
-        for (Transaction transaction: transactions) {
-            if(transaction.getDate().equals(date)){
-                if(transaction.getEMV_Scheme().equals(EMV_Scheme)) {
-                    System.out.println("Тип приложения соответствует ожидаемому");
-                } else System.out.println("Тип приложения не соответствует ожидаемому");
+        for (Transaction transaction : transactions) {
+            if (transaction.getDate().equals(date)) {
+                if (transaction.getEMV_Scheme().equals(EMV_Scheme)) {
+                    return "Тип приложения соответствует ожидаемому";
+                } else return "Тип приложения не соответствует ожидаемому";
             }
         }
+        throw new IllegalArgumentException("Value of paremeters is incorrected, such a transaction does not exist");
     }
 }
 
-class Transaction{
-    private String date="";
-    private String EMV_Scheme="";
+class Transaction {
+    private String date = "";
+    private String EMV_Scheme = "";
 
     public Transaction() {
 
@@ -77,20 +78,21 @@ class MainTwo {
         List<String> lines = new ArrayList<>();
 
         try {
-            lines = Files.readAllLines(Paths.get("C:\\Users\\Андрей\\OneDrive\\Рабочий стол\\untitled\\A.log"), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
+            lines = Files.readAllLines(Paths.get(".\\A.log"), StandardCharsets.UTF_8);
+            ExerciseTwo logA = new ExerciseTwo(lines);
+            System.out.println(logA.checkTypeOfApp("dsocjscnkdsnc", "svsfvsfv"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
 
-        ExerciseTwo logA = new ExerciseTwo(lines);
-        logA.checkTypeOfApp("2018-02-19 16:35:59.574","M/Chip Advance");
 
         try {
-            lines = Files.readAllLines(Paths.get("C:\\Users\\Андрей\\OneDrive\\Рабочий стол\\untitled\\B.log"), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
+            lines = Files.readAllLines(Paths.get(".\\B.log"), StandardCharsets.UTF_8);
+            ExerciseTwo logB = new ExerciseTwo(lines);
+            System.out.println(logB.checkTypeOfApp("2018-02-19 16:36:42.174", "Chip Test"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        ExerciseTwo logB = new ExerciseTwo(lines);
-        logB.checkTypeOfApp("2018-02-19 16:36:42.174", "Chip Test");
+
     }
 }
